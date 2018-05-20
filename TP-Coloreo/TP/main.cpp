975e1b0e2296715db9600381646a1de10acfe021
trimegisto <federico.lochbaum@gmail.com>
2018-05-20 20:07:45 -0300

    add variables of pdf

--- TP-Coloreo/TP/main.cpp
+++ TP-Coloreo/TP/main.cpp
@@ -11,8 +11,23 @@ typedef vector< pii > vii;
 typedef vector<vii> graph;
 
 int N, T, M, current, i, j, cost;
-vi frequency;
-mi cost_matrix;
+vi f; // indica el costo de usar la frecuencia i
+vi y; // 0 o 1 si la frecuencia i esta usada
+mi x; // 0 o 1 en k, i si la frecuencia k es usada por la antena i
+mi z; // 0 o 1 si para la antena i, j se asigna la misma frecuecia
+mi cost_conflict_matrix; // costo de colisiones de frecuencias para el par de antenas i, k
+
+int loss_for_collision(){
+    return 0;
+}
+
+int cost_of_frequencies() {
+    return 0;
+}
+
+int functional(graph &graph1) {
+    return cost_of_frequencies() + loss_for_collision();
+}
 
 
 graph grasp() {
@@ -34,19 +49,21 @@ void show_solution(graph graph) {
 
 
 int main() {
+    cin >> N; // cantidad de antenas
+    cin >> T; // cantidad de frecuencias
+    cin >> M; // cantidad de conflictos
 
-    cin >> N;
-    cin >> T;
-    cin >> M;
-
-    frequency = vi(T);
-    cost_matrix = vector<vi>(N, vector<int>(N));
+    y = vi(T, 0);
+    f = vi(T);
+    cost_conflict_matrix = vector<vi>(N, vector<int>(N));
 
 
     for(auto i = 0; i < T; i++) {
         cin >> current;
-        frequency.push_back(current);
+        f.push_back(current);
     }
+
+    //ordenar costo de frecuencia de menor a mayor
 
     for(auto e = 0; e < M; e++) {
         cin >> i;
@@ -56,8 +73,8 @@ int main() {
         i--; // Uso indices desde 0 hasta N-1
         j--;
 
-        cost_matrix[i][j] = cost;
-        cost_matrix[j][i] = cost;
+        cost_conflict_matrix[i][j] = cost;
+        cost_conflict_matrix[j][i] = cost;
     }
 
     show_solution(grasp());
