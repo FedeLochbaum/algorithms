42706cd5c496bfa3228e8124d14aa5a02f16d874
trimegisto <federico.lochbaum@gmail.com>
2018-05-20 14:37:35 -0300

    grasp scheme

--- TP-Coloreo/TP/main.cpp
+++ TP-Coloreo/TP/main.cpp
@@ -4,12 +4,33 @@
 
 using namespace std;
 
+typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> mi;
+typedef vector< pii > vii;
+typedef vector<vii> graph;
 
 int N, T, M, current, i, j, cost;
 vi frequency;
 mi cost_matrix;
+
+
+graph grasp() {
+    graph global_solution = greedy_solution();
+
+    while(!stop_criteria()) {
+        graph current_solution = local_search(greedy_solution());
+        if(functional(current_solution) < functional(global_solution)) {
+            global_solution = current_solution;
+        }
+    }
+    return global_solution;
+}
+
+void show_solution(graph graph) {
+
+}
+
 
 
 int main() {
@@ -38,6 +59,8 @@ int main() {
         cost_matrix[i][j] = cost;
         cost_matrix[j][i] = cost;
     }
+
+    show_solution(grasp());
 
     return 0;
 }
