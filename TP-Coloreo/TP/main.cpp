e257b77e8d8c61a06735028477589886a8690a46
trimegisto <federico.lochbaum@gmail.com>
2018-05-20 11:03:20 -0300

    add simple input file example and parse of input in main.pcc

--- TP-Coloreo/TP/main.cpp
+++ TP-Coloreo/TP/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> mi;
+
+int N, T, M, current, i, j, cost;
+vi frequency;
+mi cost_matrix;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cin >> N;
+    cin >> T;
+    cin >> M;
+
+    frequency = vi(T);
+    cost_matrix = vector<vi>(N, vector<int>(N));
+
+
+    for(auto i = 0; i < T; i++) {
+        cin >> current;
+        frequency.push_back(current);
+    }
+
+    for(auto e = 0; e < M; e++) {
+        cin >> i;
+        cin >> j;
+        cin >> cost;
+
+        i--; // Uso indices desde 0 hasta N-1
+        j--;
+
+        cost_matrix[i][j] = cost;
+        cost_matrix[j][i] = cost;
+    }
+
     return 0;
 }
