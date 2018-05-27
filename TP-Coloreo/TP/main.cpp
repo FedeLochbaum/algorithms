bda2115e1435eb668667fe1eebfe103a090214d7
trimegisto <federico.lochbaum@gmail.com>
2018-05-27 11:14:09 -0300

    greedy solution works!

--- TP-Coloreo/TP/main.cpp
+++ TP-Coloreo/TP/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <limits>
 #include <ctime>
 #include <algorithm>
 
 using namespace std;
 
-typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> mi;
 
@@ -15,14 +12,7 @@ typedef vector<vi> mi;
 struct Frequency {
     int cost_of_use;
     int number_id;
-    Frequency(int cost, int id);
 };
-
-Frequency::Frequency(int cost, int id) {
-    cost_of_use = cost;
-    number_id = id;
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////// Global variables ////////////////////////////////////////////
@@ -35,7 +25,7 @@ mi general_graph;
 vector<Frequency> f;  // indica el costo de usar la frecuencia i
 mi cost_conflict_matrix; // costo de colisiones de frecuencias para el par de antenas i, k
 
-
+///////////////////////////////////////////// Order function //////////////////////////////////////////////
 bool order_func (Frequency i, Frequency j) { return (i.cost_of_use < j.cost_of_use); }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,7 +68,6 @@ int Solution::cost_of_frequencies() {
             res += f[i].cost_of_use;
         }
     }
-
     return res;
 }
 
@@ -87,9 +76,7 @@ int Solution::functional() {
 }
 
 void Solution::show_solution() { // por ahora no escribe en files
-    for(auto i = 0; i < N; i++) {
-        cout << assings[i] << endl;
-    }
+    for(auto frec_id : assings) { cout << frec_id << endl; }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -99,20 +86,21 @@ Solution greedy_solution(int seed_vertex) {
 
     Solution sol = Solution();
 
-    Frequency freq = Frequency(0, 0); // Sacar inicializacion luego
+    Frequency freq{}; // Sacar inicializacion luego
     freq = f[0]; // Esta ordenado
     sol.y[freq.number_id] = 1;
     sol.assings[seed_vertex] = freq.number_id;
 
     for(int i = 0; i < sol.graph.size(); i++) {
-        if(i != seed_vertex){ // Si el vertice actual es distinto al que ya esta marcado.
+        if(sol.assings[i] == -1) { // Si el vertice actual no tiene frecuencia asignada
             vector<Frequency> possible_frequencies = f;
             for(int e = 0; e < sol.graph[i].size(); e++) {
-                if(sol.assings[sol.graph[i][e]] != -1) { // Si el vertice lindante ya posee un color
+                auto current_e = sol.graph[i][e];
+                if(sol.assings[current_e] != -1) { // Si el vertice lindante ya posee un color
 
-                    // Se busca la frecuencia con id = sol.assings[sol.graph[i][e]] y se elimina si no fue eliminada anteriormente
+                    // Se busca la frecuencia con id = sol.assings[sol.graph[i][e]] y se elimina
                     for(int j = 0; j < possible_frequencies.size(); j++) {
-                        if(possible_frequencies[j].number_id == sol.assings[sol.graph[i][e]]) {
+                        if(possible_frequencies[j].number_id == sol.assings[current_e]) {
                             possible_frequencies.erase(possible_frequencies.begin() + j);
                         }
                     }
@@ -147,6 +135,7 @@ Solution grasp() {
 //            global_solution = current_solution;
 //        }
 //    }
+
     return global_solution;
 }
 
@@ -155,13 +144,12 @@ int main() {
     cin >> T;
     cin >> M;
 
-    f = vector<Frequency>(T);
+    f = vector<Frequency>();
     cost_conflict_matrix = vector<vi>(N, vector<int>(N));
 
     for(auto i = 0; i < T; i++) {
         cin >> current;
-        Frequency freq = Frequency(current, i);
-        f.push_back(freq);
+        f.push_back({current, i}); // {current, i} :: Frequency
     }
 
     // Se ordena f por costo de uso
